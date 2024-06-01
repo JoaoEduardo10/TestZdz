561928296360da891ce08d14e2d974eb3b1e42f4
João Eduardo <joaoeduardoj920@gmail.com>
2024-06-01 19:39:02 -0300

    feat(Services): added to the creation of the product

---
+++ BackEnd/Infrastructure/Mapper/ProdutsMapper.cs
@@ -0,0 +1,35 @@
+﻿using Core.Domain;
+using Infrastructure.Dtos.Request;
+using Infrastructure.Entities;
+
+namespace Infrastructure.Mapper
+{
+    public class ProdutsMapper
+    {
+        public Produtos ToProdut(ProdutosEntity produtosEntity)
+        {
+            return new Produtos(
+             produtosEntity.Id,
+             produtosEntity.Name,
+             produtosEntity.Value
+             );
+        }
+
+        public Produtos ToProdut(ProdutRequestDto produtosEntity)
+        {
+            return new Produtos(
+             produtosEntity.Name,
+             produtosEntity.Value
+             );
+        }
+
+        public ProdutosEntity ToProdutEntity(Produtos produtos)
+        {
+            return new ProdutosEntity(
+             produtos.Name,
+             produtos.Value
+             );
+        }
+
+    }
+}
---
+++ BackEnd/Infrastructure/Services/CreateProdutGatewayImpl.cs
@@ -0,0 +1,35 @@
+﻿using Application.Gateway;
+using Core.Domain;
+using Infrastructure.Data;
+using Infrastructure.Entities;
+using Infrastructure.Mapper;
+
+namespace Infrastructure.Services
+{
+    public class CreateProdutGatewayImpl : ICreateProdutGateway
+    {
+        private readonly InfrastrutureDataBaseContext _Context;
+        private readonly ProdutsMapper _ProdutsMapper;
+
+        public CreateProdutGatewayImpl(InfrastrutureDataBaseContext context, ProdutsMapper produtsMapper)
+        {
+            _Context = context;
+            _ProdutsMapper = produtsMapper;
+        }
+
+        public async Task<bool> CreateProdutAsync(Produtos produtos)
+        {
+            var produtosEntitySaved = _Context.ProdutosEntity.Add(_ProdutsMapper.ToProdutEntity(produtos));
+
+            await _Context.SaveChangesAsync();
+
+            if(produtosEntitySaved == null )
+            {
+                return false;
+            }
+
+
+            return true;
+        }
+    }
+}
