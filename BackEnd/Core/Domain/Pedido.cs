de8ebb3335772f5c0b407ed3dd0002e5dbef6a5f
João Eduardo <joaoeduardoj920@gmail.com>
2024-06-01 11:49:53 -0300

    feat(Domain): adding contrutor fro Pedidos

--- BackEnd/Core/Domain/Pedido.cs
+++ BackEnd/Core/Domain/Pedido.cs
@@ -1,3 +1,6 @@
+using Core.Exceptions.enums;
+using Core.Exceptions;
+
 namespace Core.Domain
 {
     public class Pedido
@@ -5,5 +8,32 @@ namespace Core.Domain
         public int Id { get; set; }
         public Produto Produto { get; set; }
         public float Valor { get; set; }
+
+        public Pedido(int id, Produto produto, float valor)
+        {
+            Id = id;
+            Produto = produto;
+            Valor = MinValue(valor);
+        }
+
+        public Pedido(Produto produto, float valor)
+        {
+            Produto = produto;
+            Valor = MinValue(valor);
+        }
+
+        public Pedido()
+        {
+        }
+
+        private static float MinValue(float value)
+        {
+            if (value <= 0)
+            {
+                throw new ProdutoException(ErrorCodeEnum.PRO002.GetMessage(), ErrorCodeEnum.PRO002.GetCode());
+            }
+
+            return value;
+        }
     }
 }
