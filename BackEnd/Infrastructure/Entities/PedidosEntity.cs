5b13f31954a204059a9eb6e5914779e83dd5b459
João Eduardo <joaoeduardoj920@gmail.com>
2024-06-01 13:21:43 -0300

    feat(Infra..): adding entities

---
+++ BackEnd/Infrastructure/Entities/PedidosEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Entities
+{
+    public class PedidosEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProdutoId { get; set; }
+
+        [ForeignKey(nameof(ProdutoId))]
+        public ProdutosEntity Produtos { get; set; }
+
+        [Required]
+        public float Valor { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        public PedidosEntity(ProdutosEntity produtos, float valor, DateTime createdAt)
+        {
+            Produtos = produtos;
+            Valor = valor;
+            CreatedAt = createdAt;
+        }
+    }
+}
---
+++ BackEnd/Infrastructure/Entities/ProdutosEntity.cs
@@ -0,0 +1,28 @@
+﻿using Core.Domain;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Entities
+{
+    public class ProdutosEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Name {get; set; }
+
+        [Required]
+        public float Value { get; set; }
+
+        public virtual List<Pedidos> Pedidos { get; set; }
+
+        public ProdutosEntity(string name, float value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
