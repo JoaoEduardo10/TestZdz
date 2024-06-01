e8268b93b4f06fe76c90a6101985b0ad9ea1f7ab
João Eduardo <joaoeduardoj920@gmail.com>
2024-06-01 18:50:14 -0300

    feat(Infra...): Added Entity Connection Configuration

---
+++ BackEnd/Infrastructure/Data/InfrastrutureDataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class InfrastrutureDataBaseContext : DbContext
+    {
+        public InfrastrutureDataBaseContext(DbContextOptions<InfrastrutureDataBaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PedidosEntity> PedidosEntity { get; set; }
+        public DbSet<ProdutosEntity> ProdutosEntity { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- BackEnd/Infrastructure/Program.cs
+++ BackEnd/Infrastructure/Program.cs
@@ -1,6 +1,13 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<InfrastrutureDataBaseContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresDataBase"))
+);
 
 var app = builder.Build();
 
