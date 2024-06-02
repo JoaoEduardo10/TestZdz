3db439d56b41781d90287aeb5d830018f21c12d7
João Eduardo <joaoeduardoj920@gmail.com>
2024-06-01 23:51:37 -0300

    feat: adding dockerfile

--- BackEnd/Infrastructure/Program.cs
+++ BackEnd/Infrastructure/Program.cs
@@ -29,6 +29,21 @@ app.UseCors(cors =>
 }
 );
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<InfrastrutureDataBaseContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
